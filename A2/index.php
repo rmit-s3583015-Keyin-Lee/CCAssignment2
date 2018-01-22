7b99abcf665d5ec6aa39a4049b62f1b69744025a
rmit-s3583015-Keyin-Lee <s3583015@student.rmit.edu.au>
2018-01-22 18:43:58 +1100

    first commit

---
+++ A2/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome to A2 Cinema</title>
+    <h1>A2 Cinema Test</h1>
+</head>
+
+<body>
+    <nav>
+        <div class="nav">
+            <a href="#home">What's Hot</a>
+            <a href="#news">Now Showing</a>
+            <a href="#contact">Coming Soon</a>
+            <a href="#about">Book Now</a>
+            <div class="search">
+                <form action="/action_page.php">
+                    <input type="text" placeholder="Search.." name="search">
+                    <button type="submit"><i class="fa fa-search"></i>Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <div class="top10movies">
+        <h3>Top 10 Movies</h3>
+    </div>
+    <div class="promos">
+        <h3>Promos</h3>
+    </div>
+    <div class="cinema choice">
+        <h3>Cinema Choice</h3>
+    </div>
+    <div class="genres">
+        <h3>Genres to choose from</h3>
+    </div>
+</body>
+
+<footer>
+    <hr>
+    <div class="sitemap">
+            <a href="#home">About Us</a>
+            <a href="#news">Privacy Policy</a>
+            <a href="#contact">Forms and Conditions</a>
+            <div>All Rights Reserved, Copyright 2018</div>
+    </div>
+</footer>
+
+</html>
